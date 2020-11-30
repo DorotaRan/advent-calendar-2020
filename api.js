6053b1f3e22c8d0dde484dc240e47c2da7149508
Dorota Rankowska <dorota.rankowska@gmail.com>
2020-11-30 23:10:08 +0100

    local storage-first try

--- api.js
+++ api.js
@@ -1,39 +1,52 @@
 const gifts = [
-    'Cukierek',
-    'Batonik',
-    'Buziak od mamy',
-    'Buziak od taty',
-    'Guma do żucia',
-    'Niespodzianka',
-    'Coś dla ciała',
-    'Cos dla ducha',
-    'Napój',
-    'Świąteczny gadżet',
-    'Przytulas',
-    'Buziak od mamy',
-    'Buziak od taty',
-    '5 PLN na lodBuziak od mamy i taty',
+    'Candy',
+    'Chocolate bar',
+    'Kiss from mum',
+    'Kiss from dad',
+    'Chewing gum',
+    'Surprise',
+    'Something for the body',
+    'Something for the spirit',
+    'Drink',
+    'Christmas gadget',
+    'Hug',
+    '5 PLN',
 ];
+
 let daysDOM = [];
 let id;
 let day;
 let giftContainer = document.querySelector('#giftContainer');
 giftContainer.className=('');
+selectAdventDay();
 
 
 function selectAdventDay() {
     let days = [...document.querySelectorAll('.day')];
     daysDOM = days;
     days.forEach(day => {
-        day.addEventListener('click', event => {    
-            event.target.className=('past-date')
+         day.addEventListener('click', () => {  
+            day.className=('past-date');
             giftContainer.className=('gift-container');
             let gift = document.createElement('p');
             giftContainer.appendChild(gift);
             let randomIndexFromZeroToFivteen = parseInt(Math.random() * 10 % gifts.length);
             gift.innerText = gifts[randomIndexFromZeroToFivteen];
+            // localStorage.setItem('day', JSON.stringify(day.id) )
+            function selected(day) {
+                // this.clear();
+                day.style.textDecoration = 'line-through';
+                window.localStorage.setItem('textDecoration', 'line-through');
+            }
+            selected()
         })
     })
 };
 
-selectAdventDay()
+window.onload = function() {
+    if (window.localStorage.getItem('textDecoration')) {
+        document.querySelector('.day').style.textDecoration = window.localStorage.getItem('textDecoration')
+    }
+}
+
+  
